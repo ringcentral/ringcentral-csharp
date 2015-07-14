4e1e1c186bf71ac1baa486c6df44f72a2c8604e3
Paul Zolnierczyk <Paul.Zolnierczyk@iinteractive.com>
2015-07-14 12:32:10 -0500

    RCNETSDK-2 updating namespace

--- ringcentral/helper/PagerHelper.cs
+++ ringcentral/helper/PagerHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ringcentral.helper
+namespace RingCentral.helper
 {
     public class PagerHelper
     {
--- ringcentral/platform/Platform.cs
+++ ringcentral/platform/Platform.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ringcentral.platform
+namespace RingCentral.Platform
 {
     class Platform
     {
