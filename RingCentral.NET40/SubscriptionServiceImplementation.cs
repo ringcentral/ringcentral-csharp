967bcdd44586fd276fef4f96c18c6203b8953412
Paul Zolnierczyk <Paul.Zolnierczyk@iinteractive.com>
2015-07-21 17:07:55 -0500

    RCNETSDK-2 created constructor for pubnub api interface

--- RingCentral.NET40/SubscriptionServiceImplementation.cs
+++ RingCentral.NET40/SubscriptionServiceImplementation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using PubNubMessaging;
 using System.Diagnostics;
@@ -9,11 +10,16 @@ namespace RingCentral.Subscription
 {
     public class SubscriptionServiceImplementation : ISubscriptionService
     {
+        private PubNubMessaging.Core.Pubnub _pubnub;
+
+        public SubscriptionServiceImplementation(string publishKey, string subscribeKey)
+        {
+            _pubnub = new PubNubMessaging.Core.Pubnub(publishKey,subscribeKey);
+        }
+
         public void Subscribe(string channel, string channelGroup, Action<object> userCallback, Action<object> connectCallback, Action<SubscriptionError> errorCallback)
         {
-            var pubnub = new PubNubMessaging.Core.Pubnub("","");
-
-            pubnub.Subscribe<string>(channel, channelGroup, DisplaySubscribeReturnMessage, DisplaySubscribeConnectStatusMessage, DisplayErrorMessage);
+            _pubnub.Subscribe<string>(channel, channelGroup, DisplaySubscribeReturnMessage, DisplaySubscribeConnectStatusMessage, DisplayErrorMessage);
         }
 
         public void Unsubscribe(string channel, string channelGroup, Action<object> userCallback, Action<object> connectCallback, Action<object> disconnectCallback, Action<SubscriptionError> errorCallback)
