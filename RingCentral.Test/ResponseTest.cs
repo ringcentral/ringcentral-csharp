092571dff3885bb1211d38be33f50ec32213b7bb
nrobison <nathaniel.t.robison@gmail.com>
2015-08-07 16:48:23 -0700

    RCNETSDK-20 Added Revoke test and JsonBody test for response

--- RingCentral.Test/ResponseTest.cs
+++ RingCentral.Test/ResponseTest.cs
@@ -12,14 +12,24 @@ namespace RingCentral.Test
     public class ResponseTest : TestConfiguration
     {
         protected const string AccountInformationEndPoint = "/restapi/v1.0/account/";
+        protected const string AccountExtensionInformationEndPoint = "/restapi/v1.0/account/~/extension";
         [Test]
-        public void GetErrorMessageTest()
+        public void GetErrorMessage()
         {
 
             Request request = new Request(AccountInformationEndPoint + "5");
             Response result = RingCentralClient.GetPlatform().GetRequest(request);
             var errorResult =  result.GetError();
             Assert.IsNotNull(errorResult);
+        }
+
+        [Test, ExpectedException(typeof(Exception), ExpectedMessage = @"Response is not JSON")]
+        public void GetResponseNonJson()
+        {
+            Request request = new Request(AccountExtensionInformationEndPoint + "/6");
+            Response result = RingCentralClient.GetPlatform().GetRequest(request);
+            var jsonResult =  result.GetJson();
+            
         }
     }
 }
