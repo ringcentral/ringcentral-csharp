7e9ed1806108a4d85c8f4fb8b39fcbab722e82e3
Tyler Long <tyler4long@gmail.com>
2016-04-18 14:50:44 +0800

    Add Project RingCentral.CodeGen

---
+++ RingCentral.CodeGen/GenerateCommand.cs
@@ -0,0 +1,26 @@
+﻿using ManyConsole;
+using System;
+
+namespace RingCentral.CodeGen
+{
+    public class GenerateCommand : ConsoleCommand
+    {
+        public string Language;
+
+        public GenerateCommand()
+        {
+            this.IsCommand("g", "generate source code");
+            this.HasOption("l|lang=", "lang", l => Language = l);
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            if (Language == null)
+            {
+                Language = "csharp";
+            }
+            Console.WriteLine("Language is " + Language);
+            return 0;
+        }
+    }
+}
