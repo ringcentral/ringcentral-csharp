32a7c31a190df21773e7a6520026e00f217c6933
Tyler Long <tyler4long@gmail.com>
2016-04-18 13:22:27 +0800

    Add a test case for APIVersionTest

---
+++ RingCentral.Test.Real/APIVersionsTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using RingCentral.Http;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace RingCentral.Test.Real
+{
+    [TestFixture]
+    class APIVersionsTest:BaseTest
+    {
+        [Test]
+        public void BaseURL()
+        {
+            var request = new Request("/restapi/");
+            var response = platform.Get(request);
+            var uriString = (string)response.GetJson().SelectToken("apiVersions").First().SelectToken("uriString");
+            Assert.AreEqual("v1.0", uriString);
+        }
+    }
+}
