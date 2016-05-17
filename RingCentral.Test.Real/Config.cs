5bdea5fe3a62411fa19b94fcbe09610763741c73
Tyler Long <tyler4long@gmail.com>
2016-05-17 18:43:16 +0800

    Save credentials into config.json instead of environment variables.

---
+++ RingCentral.Test.Real/Config.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+
+namespace RingCentral.Test
+{
+    class Config
+    {
+        private static Config instance = null;
+        private Config() { }
+
+        public static Config Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    try
+                    {
+                        using (var sr = new StreamReader("config.json"))
+                        {
+                            var jsonData = sr.ReadToEnd();
+                            instance = JsonConvert.DeserializeObject<Config>(jsonData);
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Debug.WriteLine("config.json doesn't exist");
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public string ServerUrl
+        {
+            get
+            {
+                if (Server == "Production")
+                {
+                    return "https://platform.ringcentral.com";
+                }
+                else
+                {
+                    return "https://platform.devtest.ringcentral.com";
+                }
+            }
+        }
+
+        [JsonProperty("RC_APP_KEY")]
+        public string AppKey;
+
+        [JsonProperty("RC_APP_SECRET")]
+        public string AppSecret;
+
+        [JsonProperty("RC_APP_SERVER")]
+        public string Server;
+
+        [JsonProperty("RC_USERNAME")]
+        public string UserName;
+
+        [JsonProperty("RC_EXTENSION")]
+        public string Extension;
+
+        [JsonProperty("RC_PASSWORD")]
+        public string Password;
+    }
+}
---
+++ RingCentral.Test.Real/README.md
@@ -0,0 +1,10 @@
+﻿# RingCentral.Test.Real
+
+These are unit tests against real API Server.
+
+Because we think mock tests are not enough.
+
+
+## Before you run the tests
+
+Copy `config-sample.json` to `./bin/Debug/config.json` and edit the credentials.
