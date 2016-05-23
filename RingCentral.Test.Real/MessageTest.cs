14e08163a798f02bbf23007cc5e07bb96ce7b7e2
Tyler Long <tyler4long@gmail.com>
2016-05-23 16:34:32 +0800

    Add unit test for sending sms

---
+++ RingCentral.Test.Real/MessageTest.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace RingCentral.Test.Real
+{
+    [TestFixture]
+    class MessageTest : BaseTest
+    {
+        [Test]
+        public void SMS()
+        {
+            var subject = "hello world";
+            var requestBody = new {
+                text = subject,
+                from = new { phoneNumber = Config.Instance.Username },
+                to = new object[] { new { phoneNumber = Config.Instance.Receiver } }
+            };
+            var request = new Http.Request("/restapi/v1.0/account/~/extension/~/sms", JsonConvert.SerializeObject(requestBody));
+            var response = sdk.Platform.Post(request);
+            Assert.AreEqual(subject, response.Json["subject"].ToString());
+            Assert.AreEqual("Outbound", response.Json["direction"].ToString());
+            Assert.AreEqual("Sent", response.Json["messageStatus"].ToString());
+        }
+    }
+}
