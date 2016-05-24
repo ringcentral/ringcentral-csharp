9e51749ffccd5eff8e751c65ec83ae4316bb27df
Tyler Long <tyler4long@gmail.com>
2016-05-24 11:04:11 +0800

    Add unit test for PresenceSubscription

--- RingCentral.Test.Real/SubscriptionTest.cs
+++ RingCentral.Test.Real/SubscriptionTest.cs
@@ -9,7 +9,7 @@ namespace RingCentral.Test.Real
     class SubscriptionTest : BaseTest
     {
         [Test]
-        public void MessageStore()
+        public void MessageStoreSubscription()
         {
             var sub = new Subscription.SubscriptionServiceImplementation() { _platform = sdk.Platform };
             sub.AddEvent("/restapi/v1.0/account/~/extension/~/message-store");
@@ -30,6 +30,29 @@ namespace RingCentral.Test.Real
 
             Thread.Sleep(25000);
             Assert.AreEqual(2, count);
+            sub.Remove();
+        }
+
+        [Test]
+        public void PresenceSubscription()
+        {
+            var sub = new Subscription.SubscriptionServiceImplementation() { _platform = sdk.Platform };
+            sub.AddEvent("/restapi/v1.0/account/~/extension/~/presence");
+            sub.AddEvent("/restapi/v1.0/account/~/extension/~/message-store");
+            var count = 0;
+            sub.Subscribe((message) => {
+                count += 1;
+            }, null, null);
+
+            var dict = new Dictionary<string, dynamic> {
+                { "text", "hello world" },
+                { "from", new Dictionary<string, string> { { "phoneNumber", Config.Instance.Username} } },
+                { "to", new Dictionary<string, string>[] { new Dictionary<string, string> { { "phoneNumber", Config.Instance.Receiver } } } },
+            };
+            var request = new Http.Request("/restapi/v1.0/account/~/extension/~/sms", JsonConvert.SerializeObject(dict));
+            sdk.Platform.Post(request);
+            Thread.Sleep(15000);
+            Assert.AreEqual(1, count);
             sub.Remove();
         }
     }
