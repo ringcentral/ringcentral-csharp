6465d73ccf87242674fdf1a9ee6f247ca942aeda
Tyler Long <tyler4long@gmail.com>
2016-05-26 19:06:18 +0800

    Remove useless files

--- RingCentral/Subscription/Subscription.cs
+++ RingCentral/Subscription/Subscription.cs
@@ -26,3 +26,25 @@ namespace RingCentral.Subscription
         public string EncryptionKey { get; set; }
     }
 }
+
+/*
+
+{
+  "uri" : "https://platform.devtest.ringcentral.com/restapi/v1.0/subscription/40eeffda-e425-47f8-8127-d26dff22253c",
+  "id" : "40eeffda-e425-47f8-8127-d26dff22253c",
+  "creationTime" : "2016-05-26T10:38:15.804Z",
+  "status" : "Active",
+  "eventFilters" : [ "/restapi/v1.0/account/130829004/extension/130829004/message-store", "/restapi/v1.0/account/130829004/extension/130829004/presence" ],
+  "expirationTime" : "2016-05-26T10:53:15.808Z",
+  "expiresIn" : 899,
+  "deliveryMode" : {
+    "transportType" : "PubNub",
+    "encryption" : true,
+    "address" : "4132724593900141_7526c66b",
+    "subscriberKey" : "sub-c-b8b9cd8c-e906-11e2-b383-02ee2ddab7fe",
+    "encryptionAlgorithm" : "AES",
+    "encryptionKey" : "KfpwGgFQiUUZmsGA1VkCqw=="
+  }
+}
+
+*/
