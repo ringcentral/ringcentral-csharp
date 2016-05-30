f154597609e2f48850b0bb1dfe2d454a1582428a
Tyler Long <tyler4long@gmail.com>
2016-05-30 10:04:25 +0800

    Unit test error response

--- RingCentral.Test.Mock/ResponseTest.cs
+++ RingCentral.Test.Mock/ResponseTest.cs
@@ -12,8 +12,8 @@ namespace RingCentral.Test
         [Test]
         public void GetResponseNonJson()
         {
-            Request request = new Request(AccountExtensionInformationEndPoint + "/6");
-            ApiResponse result = sdk.Platform.Get(request);
+            var request = new Request(AccountExtensionInformationEndPoint + "/6");
+            var result = sdk.Platform.Get(request);
             var jsonResult = result.Json;
             Assert.IsNull(jsonResult);
         }
@@ -21,9 +21,17 @@ namespace RingCentral.Test
         [Test]
         public void GetErrorGoodCheckStatus()
         {
-            Request request = new Request(AccountInformationEndPoint);
-            ApiResponse result = sdk.Platform.Get(request);
+            var request = new Request(AccountInformationEndPoint);
+            var result = sdk.Platform.Get(request);
             Assert.IsNull(result.Error);
+        }
+
+        [Test, ExpectedException(typeof(ApiException), ExpectedMessage = @"Unsupported grant type")]
+        public void ErrorResponse()
+        {
+            var request = new Request(AccountExtensionInformationEndPoint + "/7");
+            var result = sdk.Platform.Get(request);
+            Assert.AreEqual(false, result.OK);
         }
     }
 }
