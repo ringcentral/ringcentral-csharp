9897dcbdade9a45d91264117277192ce6321df4d
Tyler Long <tyler4long@gmail.com>
2016-05-31 13:19:57 +0800

    Add real test for fax

--- RingCentral.Test.Real/MessageTest.cs
+++ RingCentral.Test.Real/MessageTest.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
 using NUnit.Framework;
+using RingCentral.Http;
+using System.Collections.Generic;
 
 namespace RingCentral.Test.Real
 {
@@ -20,6 +22,30 @@ namespace RingCentral.Test.Real
             Assert.AreEqual(subject, response.Json["subject"].ToString());
             Assert.AreEqual("Outbound", response.Json["direction"].ToString());
             Assert.AreEqual("Sent", response.Json["messageStatus"].ToString());
+        }
+
+        [Test]
+        public void Fax()
+        {
+            //var requestBody = JsonConvert.SerializeObject(new
+            //{
+            //    resolution = "High",
+            //    //coverIndex = 0,
+            //    to = new object[] { new { phoneNumber = Config.Instance.Receiver } }
+            //});
+
+            var requestBody = "{\"resolution\": \"High\", \"to\":[{\"phoneNumber\": \"" + Config.Instance.Receiver + "\"}]}";
+            var attachments = new List<Attachment>();
+            var textBytes = System.Text.Encoding.UTF8.GetBytes("hello fax");
+            var attachment = new Attachment(@"test.txt", "application/octet-stream", textBytes);
+            attachments.Add(attachment);
+            var attachment2 = new Attachment(@"test2.txt", "text/plain", textBytes);
+            attachments.Add(attachment2);
+            var request = new Request("/restapi/v1.0/account/~/extension/~/fax", requestBody, attachments);
+            //var body = request.HttpContent;
+            //Assert.NotNull(body);
+            var response = sdk.Platform.Post(request);
+            Assert.AreEqual(true, response.OK);
         }
     }
 }
