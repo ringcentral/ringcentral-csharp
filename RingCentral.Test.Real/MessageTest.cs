8b9738875e7494b8b3a9d2ee3eb825b50e82629a
Tyler Long <tyler4long@gmail.com>
2016-05-31 14:46:27 +0800

    Make fax test working

--- RingCentral.Test.Real/MessageTest.cs
+++ RingCentral.Test.Real/MessageTest.cs
@@ -17,7 +17,7 @@ namespace RingCentral.Test.Real
                 from = new { phoneNumber = Config.Instance.Username },
                 to = new object[] { new { phoneNumber = Config.Instance.Receiver } }
             };
-            var request = new Http.Request("/restapi/v1.0/account/~/extension/~/sms", JsonConvert.SerializeObject(requestBody));
+            var request = new Request("/restapi/v1.0/account/~/extension/~/sms", JsonConvert.SerializeObject(requestBody));
             var response = sdk.Platform.Post(request);
             Assert.AreEqual(subject, response.Json["subject"].ToString());
             Assert.AreEqual("Outbound", response.Json["direction"].ToString());
@@ -27,23 +27,20 @@ namespace RingCentral.Test.Real
         [Test]
         public void Fax()
         {
-            //var requestBody = JsonConvert.SerializeObject(new
-            //{
-            //    resolution = "High",
-            //    //coverIndex = 0,
-            //    to = new object[] { new { phoneNumber = Config.Instance.Receiver } }
-            //});
+            var requestBody = JsonConvert.SerializeObject(new
+            {
+                faxResolution = "High",
+                coverIndex = 0,
+                to = new object[] { new { phoneNumber = Config.Instance.Receiver } }
+            });
 
-            var requestBody = "{\"resolution\": \"High\", \"to\":[{\"phoneNumber\": \"" + Config.Instance.Receiver + "\"}]}";
             var attachments = new List<Attachment>();
             var textBytes = System.Text.Encoding.UTF8.GetBytes("hello fax");
             var attachment = new Attachment(@"test.txt", "application/octet-stream", textBytes);
             attachments.Add(attachment);
-            var attachment2 = new Attachment(@"test2.txt", "text/plain", textBytes);
-            attachments.Add(attachment2);
             var request = new Request("/restapi/v1.0/account/~/extension/~/fax", requestBody, attachments);
-            //var body = request.HttpContent;
-            //Assert.NotNull(body);
+            var body = request.HttpContent;
+            Assert.NotNull(body);
             var response = sdk.Platform.Post(request);
             Assert.AreEqual(true, response.OK);
         }
